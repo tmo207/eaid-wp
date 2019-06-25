aade654136ac485148af301ace68a537306065e4
Timo Jancke <timojancke@web.de>
2019-06-25 23:08:00 +0200

    feat: add commentform

---
+++ src/components/Form/CommentsForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Input from './Input';
+import BoxContainer from '../ContentBox/BoxContainer';
+import BoxElement from '../ContentBox/BoxElement';
+import Headline from '../Headline';
+import Textarea from './Textarea';
+
+import { PADDING_SMALL, WHITE, DARKBLUE_FONT } from '../../_common/config';
+
+const FormWrapper = styled.form`
+  width: 100%;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  font-weight: bold;
+  color: ${DARKBLUE_FONT};
+  padding: ${PADDING_SMALL};
+  flex-grow: 1;
+  border: none;
+  background: ${WHITE};
+`;
+
+const CommentsForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <BoxContainer>
+      <BoxElement>
+        <Headline margin={'0'}>Schreibe einen Kommentar</Headline>
+      </BoxElement>
+      <FormWrapper onSubmit={handleSubmit}>
+        <BoxElement wrap>
+          <Textarea placeholder="Kommentar*" />
+          <Input placeholder="Name*" />
+          <Input type="email" placeholder="E-Mail*" />
+        </BoxElement>
+        <div className="buttons">
+          <SubmitButton type="submit" disabled={''}>
+            Submit
+          </SubmitButton>
+        </div>
+      </FormWrapper>
+    </BoxContainer>
+  );
+};
+
+export default CommentsForm;
---
+++ src/components/Form/Input.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Error from './Error';
+
+import { DARK_BG, WHITE } from '../../_common/config';
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 1rem;
+  height: 2.5rem;
+  background: ${DARK_BG};
+  border: none;
+  color: ${WHITE};
+  outline: ${props => (props.outline ? '1px solid red' : 'none')};
+
+  &::placeholder {
+    color: #799ad6;
+    font-size: 1rem;
+  }
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 0.6rem;
+`;
+
+const Input = ({ placeholder, type }) => {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
+
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const handleChange = event => {
+    setValue(event.target.value);
+
+    if (error) {
+      handleBlur(event);
+    }
+  };
+
+  const handleBlur = event => {
+    if (event.target.value === '') {
+      setError('Required');
+    } else if (
+      type === 'email' &&
+      !emailRegex.test(event.target.value.toLowerCase())
+    ) {
+      setError('Provide valid email');
+    } else {
+      setError('');
+    }
+  };
+
+  return (
+    <InputWrapper>
+      <InputField
+        type="text"
+        value={value}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder={placeholder}
+        outline={error}
+      />
+      {error && <Error>{error}</Error>}
+    </InputWrapper>
+  );
+};
+
+Input.defaultProps = {
+  type: 'name'
+};
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(['email', 'name'])
+};
+
+export default Input;

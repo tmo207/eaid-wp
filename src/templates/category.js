ab6d35a884ea750dedba6a6245da6e933aee97c7
Timo Jancke <timojancke@web.de>
2019-06-25 23:10:20 +0200

    style: add linted files

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import PostList from '../components/PostList'
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import PostList from '../components/PostList';
 
 const Category = props => {
-  const { data, pageContext } = props
-  const { edges: posts, totalCount } = data.allWordpressPost
-  const { title: siteTitle } = data.site.siteMetadata
-  const { name: category } = pageContext
+  const { data, pageContext } = props;
+  const { edges: posts, totalCount } = data.allWordpressPost;
+  const { title: siteTitle } = data.site.siteMetadata;
+  const { name: category } = pageContext;
   const title = `${totalCount} post${
     totalCount === 1 ? '' : 's'
-  } in the “${category}” category`
+  } in the “${category}” category`;
 
   return (
     <Layout>
       <Helmet title={`${category} | ${siteTitle}`} />
       <PostList posts={posts} title={title} />
     </Layout>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
 
 export const pageQuery = graphql`
   query CategoryPage($slug: String!) {
@@ -30,7 +30,9 @@ export const pageQuery = graphql`
         title
       }
     }
-    allWordpressPost(filter: { categories: { slug: { eq: $slug } } }) {
+    allWordpressPost(
+      filter: { categories: { elemMatch: { slug: { eq: $slug } } } }
+    ) {
       totalCount
       edges {
         node {
@@ -39,4 +41,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
--- src/templates/tag.js
+++ src/templates/tag.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import PostList from '../components/PostList'
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import PostList from '../components/PostList';
 
 const Tag = props => {
-  const { data, pageContext } = props
-  const { edges: posts, totalCount } = data.allWordpressPost
-  const { title: siteTitle } = data.site.siteMetadata
-  const { name: tag } = pageContext
+  const { data, pageContext } = props;
+  const { edges: posts, totalCount } = data.allWordpressPost;
+  const { title: siteTitle } = data.site.siteMetadata;
+  const { name: tag } = pageContext;
   const title = `${totalCount} post${
     totalCount === 1 ? '' : 's'
-  } with the tag ${tag}`
+  } with the tag ${tag}`;
 
   return (
     <Layout>
       <Helmet title={`${tag} | ${siteTitle}`} />
       <PostList posts={posts} title={title} />
     </Layout>
-  )
-}
+  );
+};
 
-export default Tag
+export default Tag;
 
 export const pageQuery = graphql`
   query TagPage($slug: String!) {
@@ -30,7 +30,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allWordpressPost(filter: { tags: { slug: { eq: $slug } } }) {
+    allWordpressPost(filter: { tags: { elemMatch: { slug: { eq: $slug } } } }) {
       totalCount
       edges {
         node {
@@ -39,4 +39,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;

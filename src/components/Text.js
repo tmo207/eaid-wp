3513fe4558b43ef736eb2d6a6b5180ba490a1f72
Timo Jancke <timojancke@web.de>
2019-08-07 22:44:45 +0200

    feat(Text): add props

--- src/components/Text.js
+++ src/components/Text.js
@@ -7,11 +7,12 @@ import { HANDHELD_MQ, SMALL_MOBILE_TEXT, MOBILE_TEXT } from '../_common/config';
 const TextWrap = styled.p`
   display: ${props => (props.inline ? 'inline' : 'block')};
   font-size: ${props => (!props.secondary ? '1.4rem' : '1rem')};
-  width: 100%;
+  width: ${props => (props.contentWidth ? 'auto' : '100%')};
   margin: ${props => (props.margin ? props.margin : '0 0 1rem')};
   padding: ${props => (props.padding ? props.padding : '0')};
   font-weight: ${props => (props.bold ? 'bold' : 'normal')};
-  text-align: ${props => (props.center ? 'center' : 'start')};
+  text-align: ${props => props.align};
+  cursor: ${props => props.onClick && 'pointer'};
   color: ${props =>
     !props.secondary ? 'inherit' : 'rgba(255, 255, 255, 0.5)'};
 
@@ -26,16 +27,20 @@ const Text = ({
   padding,
   bold,
   secondary,
-  center,
-  inline
+  align,
+  inline,
+  onClick,
+  contentWidth
 }) => {
   return (
     <TextWrap
+      contentWidth={contentWidth}
+      onClick={onClick}
       padding={padding}
       margin={margin}
       bold={bold}
       secondary={secondary}
-      center={center}
+      align={align}
       inline={inline}
       dangerouslySetInnerHTML={{
         __html: children
@@ -44,14 +49,20 @@ const Text = ({
   );
 };
 
+Text.defaultProps = {
+  align: 'start'
+};
+
 Text.propTypes = {
   children: PropTypes.string,
   margin: PropTypes.string,
   padding: PropTypes.string,
   bold: PropTypes.bool,
   secondary: PropTypes.bool,
-  center: PropTypes.bool,
-  inline: PropTypes.bool
+  align: PropTypes.string,
+  inline: PropTypes.bool,
+  onClick: PropTypes.func,
+  contentWidth: PropTypes.bool
 };
 
 export default Text;

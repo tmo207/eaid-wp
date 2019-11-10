c0c1c310de79341410590a6ddf022640b1cb92ea
Timo Jancke <timojancke@web.de>
2019-11-10 22:41:28 +0100

    chore: extract gql fragment

---
+++ src/_common/gql.js
@@ -0,0 +1,35 @@
+/* eslint-disable import/prefer-default-export */
+export const pageQuery = graphql`
+  fragment PostListFields on wordpress__POST {
+    polylang_current_lang
+    id
+    title
+    excerpt
+    author {
+      name
+      slug
+    }
+    date(formatString: "DD.MM.YYYY")
+    slug
+    featured_media {
+      localFile {
+        childImageSharp {
+          fluid(maxWidth: 960, maxHeight: 600) {
+            base64
+            tracedSVG
+            aspectRatio
+            src
+            srcSet
+            srcWebp
+            srcSetWebp
+            sizes
+            originalImg
+            originalName
+            presentationWidth
+            presentationHeight
+          }
+        }
+      }
+    }
+  }
+`;
